ca97e1304d344c90aa1adab24c36ade1f8d06874
Duong <nguyenkhanhduong147@gmail.com>
2025-03-12 16:56:22 +0700

    Fix Git identity issue and update code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,39 @@
+import Button from "../components/Button";
+
+
 export default function Home() {
     return (
-      <div>
+      <div className="max-w-[1512px]">
+        <div className="content relative  flex justify-between items-center">
+          {/* Lớp phủ 1 (từ secondary xuống button-primary, 75%-25%) */}
+          <div className="absolute inset-0 bg-gradient-to-b from-secondary via-secondary via-75% to-button-primary"></div>
+
+          {/* Lớp phủ 2 (từ button-primary lên secondary, 25%-75%) */}
+          <div className="absolute inset-0 bg-gradient-to-t from-button-primary via-button-primary via-25% to-secondary opacity-50"></div>
+
+          {/* Nội dung bên trái */}
+          <div className="content_left flex flex-col space-y-4 relative z-10 w-[543px] h-[260px]  mt-[102px] mr-[11px] ml-[140px] mb-[149px] ">
+            <h2 className="font-bold text-[64px] text-white relative z-10">
+             Our Mission
+            </h2>
+            <p className="text-white font-normal relative text-[22px] ">
+              ARCF is dedicated to promoting respiratory health through research, education, and patient-focused philanthropic activities.
+            </p>
+
+            <div className="button_content flex gap-x-10 items-center">
+              <Button className="bg-white text-primary ">Apply</Button>
+              <Button className="bg-white text-primary text-">Donate</Button>
+            </div>
+          </div>
+
+          {/* Nội dung bên phải */}
+          <div className="content_right relative z-10 w-[818px] h-full ">
+            <img className="w-full h-full object-fill" src="/images/headerimg.png" alt="Header Image" />
+          </div>
+
+
+
+        </div>
        
       </div>
     );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,23 +4,26 @@ import Button from "./Button";
 
 const Header = () => {
   return (
-    <header className="w-full pt-4 flex  items-center px-[72px]">
-        {/* Logo */}
-        <div className="header_logo">
+    <header className="max-w-[1512px]">
+        {/* Top header */}
+        <div className="top_header flex  h-[158px] px-[72px] items-center justify-between">
+          {/* Logo */}
+          <div className="header_logo">
             <img src="/images/logo.png" alt="Logo" />
-        </div>
+          </div>
 
-        {/* Navigation */}
-        <nav className="flex ">
-            <a href="#" className="px-[55px] text-primary">About</a>
+          {/* Navigation */}
+          <nav className="flex ">
+            <a href="#" className=" px-[55px] text-primary">About</a>
             <a href="#" className="pr-[53px] text-primary">How We Give</a>
             <a href="#" className="pr-[54px] text-primary">Donations & Support</a>
             <a href="#" className="pr-[52px] text-primary">News</a>
             <a href="#" className="pr-[66px] text-primary">Contact</a>
-        </nav>
+          </nav>
 
-        {/* Donate Button */}
-        <Button>Donate</Button>
+          {/* Donate Button */}
+          <Button>Donate</Button>
+        </div>
     </header>
   );
 };
