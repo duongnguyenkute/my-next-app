f02fdfe1f5aeffa8fb8ca1e2ecb630badbc651ac
duongnguyenkute <nguyenkhanhduong147@gmail.com>
2025-03-16 16:15:18 +0700

    Cập nhật dự án

--- src/components/HowWeGiveItem.tsx
+++ src/components/HowWeGiveItem.tsx
@@ -10,7 +10,7 @@ const HowWeGiveItem = ({ imageSrc, title1, title2 }: HowWeGiveItemProps) => {
   return (
     <div className="flex flex-col justify-center items-center h-[170px]">
       <img className="mb-2" src={imageSrc} alt={`${title1} ${title2}`} />
-      <h4 className=" h-[60px] text-primary text-center text-[28px] font-semibold">{title1} <br /> {title2}</h4>
+      <div className=" h-[60px] text-primary text-center text-[28px] leading-tight font-semibold">{title1} <br /> {title2}</div>
       <Button className="text-primary w-[170px]  h-[48px] mt-[23px]">Apply Now</Button>
     </div>
   );
